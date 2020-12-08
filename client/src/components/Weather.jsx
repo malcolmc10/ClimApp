2c3813ad7c9b85407cecfaea48ffd4ce1b73a98f
Colin Malcolm <colinmalcolm8@gmail.com>
2020-12-07 22:48:43 -0500

    set rules for rendering data on the page

--- client/src/components/Weather.jsx
+++ client/src/components/Weather.jsx
@@ -10,16 +10,18 @@ class Weather extends React.Component {
                 {this.props.city && this.props.country &&  <p>{ this.props.city }, { this.props.country }</p>}
                 
                 {this.props.temperature && <p>Temperature:&nbsp;{ this.props.temperature }</p>}
+
+                {this.props.temp_max && this.props.temp_min && <p>High:&nbsp;{ this.props.temp_max }&nbsp;Low:&nbsp;{ this.props.temp_min }</p>}
+                
+                {this.props.temp_min && <p>L:&nbsp;{ this.props.temp_min }</p>}
                 
                 {this.props.description && <p>Conditions:&nbsp;{ this.props.description }</p>}
                 
                 {this.props.humidity && <p>Humidity:&nbsp;{ this.props.humidity }</p>}
                 
-                {this.props.feels_like && <p>Feels Like:&nbsp;{ this.props.feels_like }</p>}
+                {this.props.feels_like && <p>Feels Like:&nbsp;{ this.props.feels_like }</p>} 
                 
-                {this.props.temp_min && <p>Temp Min:&nbsp;{ this.props.temp_min }</p>} 
                 
-                {this.props.temp_max && <p>Temp Max:&nbsp;{ this.props.temp_max }</p>}
                 
 
 
